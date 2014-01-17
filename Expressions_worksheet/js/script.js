a244470090348f582a764e523c26ca2932b18929
jspruell <john.spruell@gmail.com>
2014-01-16 22:51:00 -0500

    File Prep

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,5 @@
+//John Spruell 16 January 2014 Expression Worksheet
+// JavaScript
+
+//this changes the css of your body tag to be green
+*//document.querySelector("body").style.backgroundColor= "#00FF00"; *//
