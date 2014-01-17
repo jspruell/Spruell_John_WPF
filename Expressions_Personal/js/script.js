09f05f9fe483c3c3313e141f492a4ac477acc613
jspruell <john.spruell@gmail.com>
2014-01-16 23:50:37 -0500

    a lot of math problems

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,10 @@
+//John Spruell 16 January 2014 Expressions Worksheet
+
+var width = prompt("Since we have a family to feed, we need to make steak and cheese omelettes! \nPlease enter how many eggs you have");
+var cheese = prompt("How many slices of cheese do you have? :");
+var servingsOfsteak = prompt("how many servings of steak do you have? ")
+var omelettes = cheese(1) + servingsOfsteak(1) + eggs(2) ;  //calculates with the infor
+//console.log(area);  //prints it to the console
+
+alert("You can make the following number of omelettes: "+ omelettes );
+console.log ("You can make the following number of omelettes: " + omelettes);
