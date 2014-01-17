5d202ae7b47635ff32ed0c29cff3c7191fc8513f
jspruell <john.spruell@gmail.com>
2014-01-16 23:13:36 -0500

    areo changed to area!

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,6 @@
 //John Spruell 16 January 2014 Expressions Worksheet
 
-var width = prompt("We are calculating the areo of a rectangle. \nPlease enter the width");
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height:");
 var area = width * height;  //calculates with the infor
 //console.log(area);  //prints it to the console
