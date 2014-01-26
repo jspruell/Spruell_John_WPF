82aa9e44f8d9290780d7f0cb4d1c5a761b44d259
John Spruell <john.spruell@gmail.com>
2014-01-26 15:13:32 -0800

    fixed division error, removed old template

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,17 +1,10 @@
 //John Spruell 26 January 2014 Assignment: Conditionals
 
-var eggs = prompt("Since we have a family to feed, we need to make steak and cheese omelettes! \nPlease enter how many eggs you have");
-var cheese = prompt("How many slices of cheese do you have? :");
-var servingsOfsteak = prompt("how many servings of steak do you have? ")
-var omelettes = cheese * servingsOfsteak * eggs  ;  //calculates with the infor
-//console.log(area);  //prints it to the console
-
-alert("You can make the following number of omelettes: "+ omelettes );
-console.log ("You can make the following number of omelettes: " + omelettes);
-
 
 //This calculator calculates how many wood walls can me made with wood found in a video game//
 
 var wood = prompt("How much wood do you currently have in your inventory?")
-var woodPlanks =prompt("How many wood planks do you cuurently have in your inventory?")
-var
+var woodPlanks = prompt("How many wood planks do you cuurently have in your inventory?")
+var woodWalls = (woodPlanks / 4) + (wood / 40)
+alert("You can make the following ammount of Wood Walls: " + woodWalls)
+console.log ("You can make the following ammount of wood walls : " + woodWalls);
