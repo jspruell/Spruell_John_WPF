3fd64e0e1bb49d1f824e3e9730c1b4b979d99780
John Spruell <john.spruell@gmail.com>
2014-01-26 18:07:37 -0800

    calc changes

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,25 +1,22 @@
 //John Spruell 26 January 2014 Assignment: Conditionals (Industry)
 
 
-//This calculator calculates if you have enough administrators online to support a number of end users.//
+//This calculator tells you whether or not you need to have an admin online to provide support.
 
 //User inputs ammount of administrators are currently online.
 var administrators = prompt("How many administrators do you have online?");
 //User inputs ammount of users are currently online.
 var users = prompt("How many users do you have online?");
 
-//if Ceiling is less than 1 and Walls is less than 4.
-
-
-if(woodCeiling < 1) {
+//if Administrator is less than 1 and Users is less than 50.
+if(administrators < 1) {
 	//code performed if Condition is True
-	console.log ("You Do Not Have Enough for a Wood Shelter!")
-	alert ("You Do Not Have Enough for a Wood Shelter!")
-}else if(woodWalls < 4){
+	console.log ("You need an Administrator online!")
+	alert ("You need an Administrator online!")
+}else if(users < 50){
 	//code performed if condition is True
-	console.log ("You Need More Walls")
-	alert ("You Need More Walls")
-}else{
+	console.log ("You do not need an administrator online.")
+	alert ("You do not need an administrator online.")
 	//code performed if Condition is False
 	console.log ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
 	alert ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
