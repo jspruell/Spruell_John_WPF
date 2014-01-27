ba3944a8082df0c224db69ccb1fa30f78983a76b
John Spruell <john.spruell@gmail.com>
2014-01-26 17:09:34 -0800

    Applied Template

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,10 +1,31 @@
-//John Spruell 16 January 2014 Expressions Worksheet
+//John Spruell 26 January 2014 Assignment: Conditionals
 
-var eggs = prompt("Since we have a family to feed, we need to make steak and cheese omelettes! \nPlease enter how many eggs you have");
-var cheese = prompt("How many slices of cheese do you have? :");
-var servingsOfsteak = prompt("how many servings of steak do you have? ")
-var omelettes = cheese * servingsOfsteak * eggs  ;  //calculates with the infor
-//console.log(area);  //prints it to the console
 
-alert("You can make the following number of omelettes: "+ omelettes );
-console.log ("You can make the following number of omelettes: " + omelettes);
+//This calculator calculates if you have enough Wood Walls to build a shelter in a video game.//
+
+//User inputs ammount of Wood ceilings currently in his/her inventory
+var woodCeiling = prompt("How many Wood Ceilings do you have?");
+//User inputs ammount of Wood Walls currently in his/her inventory
+var woodWalls = prompt("How many Wood Walls do you have?");
+
+//if Ceiling is less than 1 and Walls is less than 4.
+
+
+if(woodCeiling < 1) {
+	//code performed if Condition is True
+	console.log ("You Do Not Have Enough for a Wood Shelter!")
+	alert ("You Do Not Have Enough for a Wood Shelter!")
+}else if(woodWalls < 4){
+	//code performed if condition is True
+	console.log ("You Need More Walls")
+	alert ("You Need More Walls")
+}else{
+	//code performed if Condition is False
+	console.log ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
+	alert ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
+} 
+	
+
+
+//if you have 4 Wood Walls and 1 Wood Ceiling you can make a Wooden Shelter//
+//
