aad4b0360f9e79d53cc84f9e2acd87c935dcce35
John Spruell <john.spruell@gmail.com>
2014-01-30 19:30:21 -0800

    very very confused

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,24 +5,36 @@
 var kidNumber = prompt("How Old is your Child?")
 var kidMax = prompt("How Old Do You Want Them to be When They Move Out?")
 
-function getOut(age){
+function getOut(years){
 	var getOut = kidMax - kidNumber; 
 	console.log(getOut);
 }
 
-var age = getOut;
+var years = getOut;
 
-
-getOut(age);
-
-if(kidNumber > kidMax){
-	console.log("Looks Like You can't Kick them Out Just Yet")
-}else if(kidNumber < kidMax){
-	console.log("You should have kicked them out years ago!")
-}else{
+if(getOut = 0){
 	console.log("Just Old Enough to Get Out!")
+}else if(getOut > 0){
+	console.log("Looks Like You can't Kick them Out Just Yet")
+}else{
+	console.log("You should have kicked them out years ago!")
 }
 
+//if Ceiling is less than 1 and Walls is less than 4.
+//if(woodCeiling < 1) {
+	//code performed if Condition is True
+//	console.log ("You Do Not Have Enough for a Wood Shelter!")
+//	alert ("You Do Not Have Enough for a Wood Shelter!")
+//}else if(woodWalls < 4){
+	//code performed if condition is True
+//	console.log ("You Need More Walls")
+//	alert ("You Need More Walls")
+//}else{
+	//code performed if Condition is False
+//	console.log ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
+//	alert ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
+//} 
+	
 
 
 
