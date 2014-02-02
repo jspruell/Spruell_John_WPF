40faa0e91562c96d4cf9858da42df17a40e8aa94
John Spruell <john.spruell@gmail.com>
2014-02-01 17:56:36 -0800

    alert mistake fixed

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,10 +3,10 @@
 
 var total = calcSpaceDiff();//variable of total created with data that is sent from the function
 
-function calcSpaceDiff (driveSpace, updateSize){//function name, and variables involved
-	var driveSpace= prompt("How many cups of coffee do you need for your shift?");//user inputs how much space the user has available
-	var updateSize = prompt("How much coffee have you already made?"); //user inputs how large the update is
-	var spaceDiff = driveSpace - updateSize;  //calculates a value for value of spaceDiff
+function calcSpaceDiff (coffeeReq, coffeeMade){//function name, and variables involved
+	var coffeeReq= prompt("How many cups of coffee do you need for your shift?");//user inputs how much space the user has available
+	var coffeeMade = prompt("How much coffee have you already made?"); //user inputs how large the update is
+	var spaceDiff = coffeeReq - coffeeMade;  //calculates a value for value of spaceDiff
 	return spaceDiff;//sends the information out of the function
 }
 console.log(total);//displays in the console the value of the var total which is derived from the calculations of the function
@@ -18,10 +18,10 @@ if(total == 0) {
 	alert ("You have just enough coffee to keep you functioning for a shift")//notifies user of result
 }else if(total > 0){
 	//code performed if condition is True
-	console.log ("You have plenty of coffee, share some with a co-worker")//provides info to the console if the else if is followed
-	alert ("You have plenty of coffee, share some with a co-worker")//notifies user of result
+	console.log ("You better make some more coffee")//provides info to the console if the else if is followed
+	alert ("You better make some more coffee")//notifies user of result
 }else{
 	//code performed if Condition is False
-	console.log ("You better make some more coffee")//provides info to the console if the else is followed
-	alert ("You better make some more coffee")//notifies user of result
+	console.log ("You have plenty of coffee, share some with a co-worker")//provides info to the console if the else is followed
+	alert ("You have plenty of coffee, share some with a co-worker")//notifies user of result
 } 
