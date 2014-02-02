1afe919ed0b3fca9fc25f35381a44241b97c2434
John Spruell <john.spruell@gmail.com>
2014-02-01 17:21:25 -0800

    template applied

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,34 +1,27 @@
+//This calculator lets you know if your kid is available to be kicked out of the house yet
 
-//This calculator calculates how any years you have left until you can kick your kids out
 
+var total = calcKidDiff();//variable of total created with data that is sent from the function
 
-var kidNumber = prompt("How Old is your Child?")
-var kidMax = prompt("How Old Do You Want Them to be When They Move Out?")
-
-function getOut(age){
-	var getOut = kidMax - kidNumber; 
-	console.log(getOut);
+function calcKidDiff (kidAge, kidMoveout){//function name, and variables involved
+	var kidAge= prompt("How Old is your Kid?");//user inputs current age of the kid
+	var kidMoveout = prompt("When  do you want your kid to move out?"); //age that user would like the kid to move out
+	var kidDiff = kidAge - kidMoveout;  //calculates a value for value of kidDiff
+	return kidDiff;//sends the information out of the function
 }
+console.log(total);//displays in the console the value of the var total which is derived from the calculations of the function
 
-var age = getOut;
 
-
-getOut(age);
-
-if(kidNumber > kidMax){
-	console.log("Looks Like You can't Kick them Out Just Yet")
-}else if(kidNumber < kidMax){
-	console.log("You should have kicked them out years ago!")
+if(total == 0) {
+	//code performed if Condition is True
+	console.log ("Just Old enough to move out!")//provides info to the console if the if is followed
+	alert ("Just Old enough to move out!")//notifies user of result
+}else if(total > 0){
+	//code performed if condition is True
+	console.log ("He should have already been kicked out")//provides info to the console if the else if is followed
+	alert ("He should have already been kicked out")//notifies user of result
 }else{
-	console.log("Just Old Enough to Get Out!")
-}
-
-
-
-
-
-
-
-
-
-
+	//code performed if Condition is False
+	console.log ("Looks like you are stuck with them for a little while longer!")//provides info to the console if the else is followed
+	alert ("Looks like you are stuck with them for a little while longer!")//notifies user of result
+} 
