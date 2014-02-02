1d4bdb182ea4e5cf3d80b4bb5d06015c7e20f574
John Spruell <john.spruell@gmail.com>
2014-02-01 16:07:53 -0800

    Working

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,25 +1,84 @@
-//John Spruell 26 January 2014 Assignment: Conditionals (Wacky)
+//This calculator calculates how any years you have left until you can kick your kids out
+
+/*function calcArea (){
+	var width= 20;
+	var height = prompt("What is the height?");
+	var area = width * height;
+	console.log(area);
+}
+
+calcArea();
+calcArea();
 
 
-//This calculator calculates if you have enough Wood Walls to build a shelter in a video game.//
+//This calculator calculates how any years you have left until you can kick your kids out
 
-//User inputs ammount of Wood ceilings currently in his/her inventory
-var woodCeiling = prompt("How many Wood Ceilings do you have?");
-//User inputs ammount of Wood Walls currently in his/her inventory
-var woodWalls = prompt("How many Wood Walls do you have?");
+var kidNumber = prompt("How Old is your Child?")//age of child in question
+var kidMax = prompt("How Old Do You Want Them to be When They Move Out?")//Prefered age of move out
+var total = calcLeave(kidMax, kidNumber);
+
+function calcLeave(kidMax, kidNumber){
+	var timeToLeave = kidMax - kidNumber; 
+	return timeToLeave; //function sending the info out
+}
+console.log(total);
+
+if(total = kidMax){
+	console.log("Just Old Enough to Get Out!")
+}else if(total < kidMax){
+	console.log("Looks Like You can't Kick them Out Just Yet")
+}else{
+	console.log("You should have kicked them out years ago!")
+}
 
 //if Ceiling is less than 1 and Walls is less than 4.
-if(woodCeiling < 1) {
+//if(woodCeiling < 1) {
 	//code performed if Condition is True
-	console.log ("You Do Not Have Enough for a Wood Shelter!")
-	alert ("You Do Not Have Enough for a Wood Shelter!")
-}else if(woodWalls < 4){
+//	console.log ("You Do Not Have Enough for a Wood Shelter!")
+//	alert ("You Do Not Have Enough for a Wood Shelter!")
+//}else if(woodWalls < 4){
 	//code performed if condition is True
-	console.log ("You Need More Walls")
-	alert ("You Need More Walls")
-}else{
+//	console.log ("You Need More Walls")
+//	alert ("You Need More Walls")
+//}else{
 	//code performed if Condition is False
-	console.log ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
-	alert ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
-} 
+//	console.log ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
+//	alert ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
+//} 
 	
+
+
+
+
+function calcAge(kidAge, kidTwo){
+	var total = kidOne + kidTwo
+	return total; //function sending kid One and Kid Two's age out
+}
+console.log(total);
+
+if(xxxxxxxx xxxxxxxx){
+	console.log("Just Old Enough to Get Out!")
+}else if(total < kidMax){
+	console.log("Looks Like You can't Kick them Out Just Yet")
+}else{
+	console.log("You should have kicked them out years ago!")
+}
+*/
+function calcKids (){
+	var kidOne = prompt("How old is kid One")//Age of kid one
+	var finalAge = prompt("How old is kid Two")//Age of kid two
+	var kidDiff = finalAge * kidOne
+	console.log(area);
+}
+
+kidDiff();
+
+
+/*if(xxxxxxxx xxxxxxxx){
+	console.log("Just Old Enough to Get Out!")
+}else if(total < kidMax){
+	console.log("Looks Like You can't Kick them Out Just Yet")
+}else{
+	console.log("You should have kicked them out years ago!")
+}
+
--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,73 +1,22 @@
-//This calculator calculates how any years you have left until you can kick your kids out
+//This calculator
 
-/*function calcArea (){
-	var width= 20;
-	var height = prompt("What is the height?");
-	var area = width * height;
-	console.log(area);
+function calcKidDiff (){
+	var kidAge= prompt("How Old is your Kid?");
+	var kidMoveout = prompt("When  do you want your kid to move out?");
+	var kidDiff = kidAge - kidMoveout;
+	console.log(kidDiff);
 }
 
-calcArea();
-calcArea();
-*/
+calcKidDiff();
 
-//This calculator calculates how any years you have left until you can kick your kids out
 
-var kidNumber = prompt("How Old is your Child?")//age of child in question
-var kidMax = prompt("How Old Do You Want Them to be When They Move Out?")//Prefered age of move out
-var total = calcLeave(kidMax, kidNumber);
-
-function calcLeave(kidMax, kidNumber){
-	var timeToLeave = kidMax - kidNumber; 
-	return timeToLeave; //function sending the info out
-}
-console.log(total);
-
-if(total = kidMax){
+if( calcKidDiff > 0){
 	console.log("Just Old Enough to Get Out!")
-}else if(total < kidMax){
+}
+else if( calcKidDiff < 0){
 	console.log("Looks Like You can't Kick them Out Just Yet")
-}else{
+}
+else{
 	console.log("You should have kicked them out years ago!")
 }
-
-//if Ceiling is less than 1 and Walls is less than 4.
-//if(woodCeiling < 1) {
-	//code performed if Condition is True
-//	console.log ("You Do Not Have Enough for a Wood Shelter!")
-//	alert ("You Do Not Have Enough for a Wood Shelter!")
-//}else if(woodWalls < 4){
-	//code performed if condition is True
-//	console.log ("You Need More Walls")
-//	alert ("You Need More Walls")
-//}else{
-	//code performed if Condition is False
-//	console.log ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
-//	alert ("You Have Enough Walls and Ceiling to for a Wood Shelter!")
-//} 
-	
-
-
-
-//If you have two kids born on the same day, how many candles do you need for their birthday cakes?
-var kidOne = prompt("How old is kid One")//Age of kid one
-var kidTwo = prompt("How old is kid Two")//Age of kid two
-var onHand = prompt("How many candles do you have on hand?")
-var total = calcAge
-
-function calcAge(kidAge, kidTwo){
-	var total = kidOne + kidTwo
-	return total; //function sending kid One and Kid Two's age out
-}
-console.log(total);
-
-if(xxxxxxxx xxxxxxxx){
-	console.log("Just Old Enough to Get Out!")
-}else if(total < kidMax){
-	console.log("Looks Like You can't Kick them Out Just Yet")
-}else{
-	console.log("You should have kicked them out years ago!")
-}
-
-
 
