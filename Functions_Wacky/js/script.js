edc0648cad8a215e2a943bdcb22d205befba5af1
John Spruell <john.spruell@gmail.com>
2014-02-01 17:47:09 -0800

    New alerts

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,4 +1,4 @@
-//This calculator lets you know if you have enough space to add more files to your web server
+//This calculator lets you know if you have enough coffee left over to keep you awake for a 12 hour shift
 
 
 var total = calcSpaceDiff();//variable of total created with data that is sent from the function
@@ -14,14 +14,14 @@ console.log(total);//displays in the console the value of the var total which is
 
 if(total == 0) {
 	//code performed if Condition is True
-	console.log ("Just enough space to install the update!")//provides info to the console if the if is followed
-	alert ("Just enough space to install the update!")//notifies user of result
+	console.log ("You have just enough coffee to keep you functioning for a shift")//provides info to the console if the if is followed
+	alert ("You have just enough coffee to keep you functioning for a shift")//notifies user of result
 }else if(total > 0){
 	//code performed if condition is True
-	console.log ("You have plenty of space, go ahead and proceed")//provides info to the console if the else if is followed
-	alert ("You have plenty of space, go ahead and proceed")//notifies user of result
+	console.log ("You have plenty of coffee, share some with a co-worker")//provides info to the console if the else if is followed
+	alert ("You have plenty of coffee, share some with a co-worker")//notifies user of result
 }else{
 	//code performed if Condition is False
-	console.log ("You are going to have to clear some space")//provides info to the console if the else is followed
-	alert ("You are going to have to clear some space")//notifies user of result
+	console.log ("You better make some more coffee")//provides info to the console if the else is followed
+	alert ("You better make some more coffee")//notifies user of result
 } 
